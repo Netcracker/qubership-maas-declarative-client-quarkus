94bc57fb23d30731220e5a79fa0c96c874887227
Evgenii Stetsenko <i@estetsenko.com>
2025-09-15 17:22:29 +0300

    fix: migrate to testcontainers (#48)

--- maas-kafka-quarkus-client/deployment/src/test/java/com/netcracker/maas/declarative/kafka/quarkus/client/test/TestConfiguration.java
+++ maas-kafka-quarkus-client/deployment/src/test/java/com/netcracker/maas/declarative/kafka/quarkus/client/test/TestConfiguration.java
@@ -2,35 +2,32 @@ package com.netcracker.maas.declarative.kafka.quarkus.client.test;
 
 import com.google.common.collect.ImmutableMap;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
-import net.mguenther.kafka.junit.EmbeddedKafkaCluster;
-import net.mguenther.kafka.junit.EmbeddedKafkaClusterConfig;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.Map;
 
-import static net.mguenther.kafka.junit.EmbeddedKafkaCluster.provisionWith;
-import static net.mguenther.kafka.junit.EmbeddedKafkaClusterConfig.defaultClusterConfig;
-
+@Testcontainers
 public class TestConfiguration implements QuarkusTestResourceLifecycleManager {
 
-    private EmbeddedKafkaCluster kafka;
+    @Container
+    private static final KafkaContainer KAFKA_CONTAINER = new KafkaContainer(DockerImageName.parse("apache/kafka"));
 
     @Override
     public Map<String, String> start() {
-        EmbeddedKafkaClusterConfig kafkaClusterConfig = defaultClusterConfig();
-        kafka = provisionWith(kafkaClusterConfig);
-        kafka.start();
+        KAFKA_CONTAINER.start();
         return ImmutableMap.<String, String>builder()
                 .put("quarkus.consul-source-config.agent.url", "localhost:8080")
                 .put("maas.kafka.local-dev.enabled", "true")
                 .put("maas.kafka.local-dev.tenant-ids", "00000000-0000-0000-0000-000000000000")
-                .put("maas.kafka.local-dev.config.bootstrap.servers", "localhost:9092")
+                .put("maas.kafka.local-dev.config.bootstrap.servers", KAFKA_CONTAINER.getBootstrapServers())
                 .build();
     }
 
     @Override
     public void stop() {
-        if (kafka != null) {
-            kafka.stop();
-        }
+        KAFKA_CONTAINER.stop();
     }
 }
