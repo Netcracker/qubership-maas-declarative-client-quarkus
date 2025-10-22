df464add3ef22e05f8e2a7adc51f67ddb3d005a7
Evgenii Stetsenko <evgenii.stetsenko.qubership@gmail.com>
2025-10-22 15:37:25 +0300

    chore: increase test coverage

--- maas-kafka-quarkus-client/runtime/src/main/java/com/netcracker/maas/declarative/kafka/quarkus/client/ConfigUtils.java
+++ maas-kafka-quarkus-client/runtime/src/main/java/com/netcracker/maas/declarative/kafka/quarkus/client/ConfigUtils.java
@@ -16,49 +16,31 @@ public class ConfigUtils {
             String key = propKey.replace("_", ".");
             if (key.startsWith(prefix)) {
                 String mapKey = key.substring(prefix.length());
-                try {
-                    Optional<Integer> intVal = config.getOptionalValue(key, Integer.class);
-                    if (intVal.isPresent() && intVal.get() instanceof Integer) {
-                        map.put(mapKey, intVal.get());
-                        continue;
-                    }
-                } catch (IllegalArgumentException | ClassCastException ex) {
-                }
-
-                try {
-                    Optional<Double> doubleVal = config.getOptionalValue(key, Double.class);
-                    if (doubleVal.isPresent() && doubleVal.get() instanceof Double) {
-                        map.put(mapKey, doubleVal.get());
-                        continue;
-                    }
-                } catch (IllegalArgumentException | ClassCastException ex) {
-                }
-
-                try {
-                    Optional<String> strVal = config.getOptionalValue(key, String.class);
-                    if (strVal.isPresent()) {
-                        String val = strVal.get().trim();
-                        if (val.equalsIgnoreCase("false")) {
-                            map.put(mapKey, false);
-                        } else if (val.equalsIgnoreCase("true")) {
-                            map.put(mapKey, true);
-                        } else {
-                            map.put(mapKey, val);
-                        }
-                        continue;
-                    }
-                } catch (IllegalArgumentException | ClassCastException ex) {
-                }
-
-                try {
-                    Optional<Boolean> boolVal = config.getOptionalValue(key, Boolean.class);
-                    boolVal.ifPresent(v -> map.put(mapKey, boolVal));
-                } catch (IllegalArgumentException | ClassCastException ex) {
-                }
+                putIfPresent(config, key, mapKey, map, Integer.class, Long.class, Double.class, Boolean.class);
+                getOptionalPropertyOfType(config, key, String.class)
+                        .map(String::trim)
+                        .ifPresent(val -> map.put(mapKey, switch(val.toLowerCase()) {
+                            case "true" -> true;
+                            case "false" -> false;
+                            default -> val;
+                        }));
             }
         }
 
         return map;
     }
 
+    private static <T> Optional<T> getOptionalPropertyOfType(Config config, String key, Class<T> propertyType) {
+        try {
+            return config.getOptionalValue(key, propertyType);
+        } catch (IllegalArgumentException | ClassCastException ex) {
+            return Optional.empty();
+        }
+    }
+
+    private static void putIfPresent(Config config, String key, String mapKey, Map<String, Object> map, Class<?>... types) {
+        for (Class<?> type : types) {
+            getOptionalPropertyOfType(config, key, type).ifPresent(value -> map.put(mapKey, value));
+        }
+    }
 }
